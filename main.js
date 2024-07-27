c1b8166e942a643c33364b760cf28cbaba83b6a2
James Kemboi <jameskemich@gmail.com>
2024-07-28 01:00:45 +0300

    modify css and  js

--- main.js
+++ main.js
@@ -10,14 +10,10 @@ function luckyNumber(value) {
 }
 
 function errorMessage(input) {
-    if (input === undefined || input === null || input === '') {
+    if (!input) 
         return 'Required field';
-    }
-    const num = Number(input);
-    if (isNaN(num) || num === 0) {
-        return 'Must be a number besides 0';
-    }
-    return '';
+   
+     return Number(input) ? '' : 'Must be a number besides 0';
 }
 
 function calculateSum() {
